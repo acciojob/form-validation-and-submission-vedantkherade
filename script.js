fee2561847bab458c1f09ec8ffbdd86354f23e07
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-11-12 17:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-//your JS code here. If required.
-    const form = document.getElementById("userForm");
-    const submitBtn = document.getElementById("submitBtn");
-    const checkbox = document.querySelector('input[name="terms"]');
+// your JS code here
+const form = document.getElementById("userForm");
+const submitBtn = document.getElementById("submitBtn");
+const checkbox = document.querySelector('input[name="terms"]');
 
-    checkbox.addEventListener("change", () => {
-      submitBtn.disabled = !checkbox.checked;
-    });
+checkbox.addEventListener("change", () => {
+  submitBtn.disabled = !checkbox.checked;
+});
 
- 
-    form.addEventListener('submit', (e) => {
-      const name = form.name.value.trim();
-      const email = form.email.value.trim();
-      const password = form.password.value.trim();
-      const gender = form.gender.value;
 
-      if (!name || !email || !password || !gender) {
-        e.preventDefault();
-        alert("Please complete all fields before submitting.");
-        return;
-      }
+form.addEventListener("submit", (e) => {
+  const name = form.name.value.trim();
+  const email = form.email.value.trim();
+  const password = form.password.value.trim();
+  const gender = form.gender.value;
 
-      if (!checkbox.checked) {
-        e.preventDefault();
-        alert("You must accept the terms and conditions.");
-        return;
-      }
 
-      alert("Form submitted successfully!");
-    });
+  if (!name || !email || !password || !gender) {
+    e.preventDefault();
+    alert("Please complete all fields before submitting.");
+    return;
+  }
+
+
+  if (!checkbox.checked) {
+    e.preventDefault();
+    alert("You must accept the terms and conditions.");
+    return;
+  }
+
+});
